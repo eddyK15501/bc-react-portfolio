04c63b20f936c87863df1532cd32804ce6571509
Edward Kim <itgma1231@gmail.com>
2023-10-16 17:48:35 -0400

    bs-Navbar.jsx bootstrap class modifications

--- src/components/bs-Navbar.jsx
+++ src/components/bs-Navbar.jsx
@@ -4,16 +4,16 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const bsNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-primary">
       <Container>
-        <Navbar.Brand href="#home">React</Navbar.Brand>
+        <Navbar.Brand className="fs-1 text-light" href="#home">React</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#about-me">About Me</Nav.Link>
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#contact-me">Contact Me</Nav.Link>
-            <Nav.Link href="#resume">Resume</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav text-light">
+          <Nav className="ms-auto fs-6 flex-gap">
+            <Nav.Link className="mx-auto text-light" href="#about-me">About Me</Nav.Link>
+            <Nav.Link className="mx-auto text-light" href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link className="mx-auto text-light" href="#contact-me">Contact Me</Nav.Link>
+            <Nav.Link className="mx-auto text-light" href="#resume">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
