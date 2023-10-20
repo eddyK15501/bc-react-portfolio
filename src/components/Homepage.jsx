a484edfb1430879688c0ad035b7dd6daded44c4e
Edward Kim <itgma1231@gmail.com>
2023-10-20 14:02:53 -0400

    npm install react-text-transition; Homepage.jsx completed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,15 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import '../sass/Homepage.scss'
+import { useState, useEffect } from "react";
+import "../sass/Homepage.scss";
+import TextTransition, { presets } from "react-text-transition";
+
+const TEXTS = ["Frontend Web Development", "Reimagined"];
 
 const Homepage = () => {
-  return (
-    <div className="container-fluid d-flex justify-content-center align-items-center" id="homepage-background">
-        <div className="">
-            <h1>React Portfolio</h1>
-        </div>
-    </div>
-  )
-}
+  const [index, setIndex] = useState(0);
 
-export default Homepage
+  useEffect(() => {
+    const intervalId = setInterval(() => setIndex((prev) => prev + 1), 3000);
+    return () => clearTimeout(intervalId);
+  }, []);
+
+  return (
+    <div
+      className="container-fluid d-flex flex-column justify-content-center align-items-center"
+      id="homepage-background"
+    >
+      <div className="d-flex flex-column">
+        <h1 className="display-4 fw-normal text-primary-emphasis">
+          React Portfolio
+        </h1>
+      <div className="mt-1 align-self-center">
+        <TextTransition
+          className="fs-5 text-secondary"
+          springConfig={presets.wobbly}
+          >
+          {TEXTS[index % TEXTS.length]}
+        </TextTransition>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Homepage;
