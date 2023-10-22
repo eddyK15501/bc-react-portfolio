24daedec8c6feb2ec923a27db2e10e3553212c3e
Edward Kim <itgma1231@gmail.com>
2023-10-21 22:59:40 -0400

    Portfolio.jsx projects static img to gif on hover

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,15 @@ import { Col, Container, Row } from "react-bootstrap";
 import '../sass/Portfolio.scss';
 
 const Portfolio = () => {
-  
+  const [hover, setHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHover(true);
+  }
+
+  const handleMouseLeave = () => {
+    setHover(false);
+  }
 
   return (
       <Container fluid className="vw-vh">
@@ -14,7 +22,7 @@ const Portfolio = () => {
           <Container className="d-flex justify-content-center align-items-center">
             <Row className="gx-5 justify-content-center text-center">
               <Col lg={4} className="d-flex justify-content-center">
-                <div id="img-container">
+                <div className="img-container">
 
                   <a href="https://eddyk15501.github.io/portfolio/">
                     <img src="" alt="project-one" />
@@ -24,23 +32,25 @@ const Portfolio = () => {
                 </div>
               </Col>
               <Col lg={4} className="d-flex justify-content-center">
-                <div id="img-container">
+                <div className="img-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
 
-                  <a href="https://eddyk15501.github.io/unc-bootcamp-project-01/">
-                    <img src="https://user-images.githubusercontent.com/88423414/277141422-189d5995-0d64-4035-b500-1b1f35543335.png" alt="project-two-image" />
-                    <img src="https://user-images.githubusercontent.com/88423414/277140769-6ec20297-6bd3-46aa-87e3-594377d1518b.gif" alt="project-two-gif" />
-                  </a>
+                  <div className="img-contain position-relative">
+                    <a href="https://eddyk15501.github.io/unc-bootcamp-project-01/">
+                      <img className={hover ? 'static-image' : ''} src="https://user-images.githubusercontent.com/88423414/277141422-189d5995-0d64-4035-b500-1b1f35543335.png" alt="project-two-image" />
+                      <img className={`gif ${hover ? '' : 'gif-image'}`} src="https://user-images.githubusercontent.com/88423414/277140769-6ec20297-6bd3-46aa-87e3-594377d1518b.gif" alt="project-two-gif" />
+                    </a>
+                  </div>
 
                   <p>Gif Me</p>
                 </div>
               </Col>
               <Col lg={4} className="d-flex justify-content-center">
-                <div id="img-container">
+                <div className="img-container">
 
                   <a href="https://eddyk15501-todolist-fullstack-10e1ec26e1dc.herokuapp.com/">
                     <img src="" alt="project-three" />
                   </a>
-                  
+
                   <p>Todolist-Fullstack</p>
                 </div>
               </Col>
