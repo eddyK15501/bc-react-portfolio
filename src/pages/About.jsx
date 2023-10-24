030971950e482c99128c2f8b13085b37549c8a70
Edward Kim <itgma1231@gmail.com>
2023-10-24 12:58:14 -0400

    About.jsx bio written; @media screen ready

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,48 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 const About = () => {
-  return ( 
-    <Container fluid className="vw-vh d-flex justify-content-center align-items-center">
-        <Container className="d-flex justify-content-center align-items-center">
-            <Row className="w-100 gx-5 fg-2">
-                <Col lg={4} className="d-flex justify-content-center">
-                    <div className="img-container">
-                        <a href="https://github.com/eddyK15501/react-portfolio-bc/issues/1">
-                            <img src="https://user-images.githubusercontent.com/88423414/277078695-7f115a5c-ec72-4038-997f-55606e4dd8d6.jpg" alt="portfolio-image" />
-                        </a>
-                    </div>
-                </Col>
-                <Col lg={5} className="d-flex flex-column justify-content-center text-center text-lg-start">
-                    <h1>Heading</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt sint ab dolorum enim ut pariatur architecto optio debitis tempora, delectus ad suscipit! Doloremque modi tempore, cum aperiam illum, nesciunt voluptate quo magnam facilis natus voluptas minus perspiciatis neque ipsam minima, ab id eius libero a debitis. Quibusdam quidem culpa voluptas?</p>
-                </Col>
-            </Row>
-        </Container>
+  return (
+    <Container
+      fluid
+      className="vw-vh d-flex justify-content-center align-items-center"
+    >
+      <Container className="d-flex justify-content-center align-items-center">
+        <Row className="w-100 gx-5 fg-2">
+          <Col lg={4} className="d-flex justify-content-center">
+            <div className="img-container">
+              <a href="https://github.com/eddyK15501/react-portfolio-bc/issues/1">
+                <img
+                  src="https://user-images.githubusercontent.com/88423414/277078695-7f115a5c-ec72-4038-997f-55606e4dd8d6.jpg"
+                  alt="portfolio-image"
+                />
+              </a>
+            </div>
+          </Col>
+          <Col
+            lg={5}
+            className="d-flex flex-column justify-content-center text-center text-lg-start"
+          >
+            <h3 className="mb-3">👋 Hi there.</h3>
+            <p className="mb-3">
+              I'm Edward. I'm a frontend web developer based out of Chapel Hill,
+              North Carolina. My objective is to create visually stunning web
+              applications that captivate users and elevate their online
+              experience.
+            </p>
+            <p className="mb-4">Anyway, get to know my a little bit. I'm a just fun
+              guy who loves to code, explore new technologies, and create
+              amazing web experiences. Whether it's solving complex coding
+              challenges or embarking on outdoor adventures, I'm always up for a
+              good challenge. If you're looking for a frontend aficionado who brings
+              enthusiasm and creativity to every project, you've come across the right
+              guy.</p>
+            <h5 className="fw-bold text-end me-5" style={{ fontFamily: 'Over the Rainbow, cursive' }}>Cheers.</h5>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
